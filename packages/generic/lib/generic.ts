b46c765e7e0396a06680f7bd661b67d7d7f17491
Arsen A. Hutsal <aahutsal@gmail.com>
2022-01-28 10:08:12 +0200

    cluster replaced with worker_threads

--- packages/generic/lib/generic.ts
+++ packages/generic/lib/generic.ts
@@ -1,14 +1,10 @@
 'use strict';
-import ccxt, { Exchange } from 'ccxt';
-import * as fs from 'fs/promises';
-import { log } from 'console';
-import pLimit from 'p-limit'
+import pAll from 'p-all'
+const { goAsync } = require('./worker')
+// Workers (threads) are useful for performing CPU-intensive JavaScript operations. They do not help much with I/O-intensive work. The Node.js built-in asynchronous I/O operations are more efficient than Workers can be.
 
-import cluster from 'cluster';
-import { cpus } from 'os';
-import process from 'process';
+// Unlike child_process or cluster, worker_threads can share memory. They do so by transferring ArrayBuffer instances or sharing SharedArrayBuffer instances.
 
-const numCPUs = cpus().length;
 const exPool = ['exmo',
     'hitbtc3',
     'hollaex',
@@ -16,52 +12,7 @@ const exPool = ['exmo',
     'oceanex',
     'upbit']
 
-
-async function go(ex:Exchange) {
-    await fs.mkdir(`${ex.id}/orderbooks`, { recursive: true });
-    await ex.loadMarkets()
-    const limit = pLimit(3);
-    const runners = Object.keys(ex.markets).map(market => limit(() => ex.fetchOrderBook(market)))
-
-    if (!ex.has.fetchOrderBooks) {
-        ex.fetchOrderBooks = () => Promise.all(runners);
-    }
-    while (true) {
-        log(`Fetching next tick ${ex.id}`)
-        await ex.fetchOrderBooks()
-            .then((obs) =>
-                fs.writeFile(`${ex.id}/orderbooks/${Date.now()}.json`, JSON.stringify(obs))
-            )
-    }
-}
+Promise.all(exPool.map((name) => goAsync(name)), // {concurrency: exPool.length}
+           )
 
 
-(async () => {
-    const EXCHANGES = ['exmo']
-    if (cluster.isPrimary) {
-        console.log(`Primary ${process.pid} is running`);
-
-        // Fork workers.
-        while(exPool.length) {
-            var proc = cluster.fork();
-            proc.on('message', async (ex: Exchange) => {
-                log(`Runnine ${ex.id} in separate thread`);
-                await go(ex)
-            })
-            proc.send(exPool.pop()) // sending next Exchange to process
-        }
-
-        cluster.on('exit', (worker, code, signal) => {
-            console.log(`worker ${worker.process.pid} died`);
-        });
-    } else {
-        // Workers can share any TCP connection
-        // In this case it is an HTTP server
-
-        console.log(`Worker ${process.pid} started`);
-        process.on('message', async (ex: string) => {
-            log(`Runnine ${ex} in separate thread`);
-            await go(new ccxt[ex])
-        })
-    }
-})()
---
+++ packages/generic/lib/worker.ts
@@ -0,0 +1,51 @@
+import ccxt, { Exchange } from 'ccxt';
+import * as fs from 'fs/promises';
+import { log, error } from 'console';
+import pLimit from 'p-limit'
+import process from 'process'
+
+import { Worker, isMainThread, parentPort, workerData } from 'worker_threads';
+
+async function go(ex: Exchange) {
+    await fs.mkdir(`${ex.id}/orderbooks`, { recursive: true });
+    await ex.loadMarkets()
+    const limit = pLimit(3);
+    const runners = Object.keys(ex.markets).map(market => limit(() => ex.fetchOrderBook(market)))
+
+    if (!ex.has.fetchOrderBooks) {
+        ex.fetchOrderBooks = () => Promise.all(runners);
+    }
+    while (true) {
+        log(`Fetching next tick ${ex.id}`)
+        await ex.fetchOrderBooks()
+            .then((obs) =>
+                fs.writeFile(`${ex.id}/orderbooks/${Date.now()}.json`, JSON.stringify(obs))
+            )
+    }
+}
+async function goAsync(exchange: string) {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(__filename, {
+            workerData: exchange
+        });
+        worker.on('message', resolve);
+        worker.on('error', (err:Error) => {
+            error(err);
+            reject(err)
+        });
+        worker.on('exit', (code) => {
+            if (code !== 0)
+                reject(new Error(`Worker stopped with exit code ${code}`));
+        });
+    });
+}
+
+if (isMainThread) {
+    log(`Primary ${process.pid} is running`);
+    module.exports = {goAsync}
+} else {
+    const exName = workerData;
+    log(`Runnine ${exName} in separate thread`);
+    go(new ccxt[exName]).then(data => parentPort.postMessage(data))
+}
+
